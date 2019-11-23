5dc2c16d3532197f60c72f004a80d4a213bdab9a
A Ishikawa <a.ishikawa3@gmail.com>
2019-11-23 23:07:52 +0900

    feat: Transpose output of 'sample'

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,6 +1,7 @@
 import random
 from collections import deque
 from typing import List, Tuple
+import numpy as np
 
 
 class ReplayBuffer:
@@ -11,8 +12,10 @@ class ReplayBuffer:
     def add(self, state, action, reward, next_state, done) -> None:
         self.memory.append((state, action, reward, next_state, done))
 
-    def sample(self, batch_size: int) -> List[Tuple]:
-        return random.sample(self.memory, batch_size)
+    def sample(self, batch_size: int) -> Tuple:
+        experiences = random.sample(self.memory, batch_size)
+        experiences = np.array(experiences)
+        return experiences[: 0], experiences[:, 1], experiences[:, 2], experiences[:, 3], experiences[:, 4]
 
     def __len__(self):
         return len(self.memory)
