c4f07bfa835cc92f50283cf90321764401dc206b
A Ishikawa <a.ishikawa3@gmail.com>
2019-12-01 22:25:38 +0900

    feat: Plot scores

--- dqn.py
+++ dqn.py
@@ -3,6 +3,8 @@ import numpy as np
 from dqn_agent import DqnAgent
 from collections import deque
 
+import matplotlib.pyplot as plt
+
 
 def main():
     n_episodes = 2000
@@ -13,6 +15,7 @@ def main():
     agent = DqnAgent(state_size=env.observation_space.shape[0], action_size=env.action_space.n)
 
     scores = []
+    moving_scores = []
     scores_window = deque(maxlen=100)
 
     for i_episode in range(1, n_episodes+1):
@@ -29,8 +32,23 @@ def main():
 
         scores_window.append(score)
         scores.append(score)
+        moving_score = np.mean(scores_window)
+        moving_scores.append(moving_score)
+
         end = '\n' if i_episode % 100 == 0 else ''
-        print(f'\rEpisode {i_episode}\tAverage Score: {np.mean(scores_window):.2f}', end=end)
+        print(f'\rEpisode {i_episode}\tAverage Score: {moving_score:.2f}', end=end)
+
+    plot_scores([scores, moving_scores])
+
+
+def plot_scores(scores_list):
+    n = np.arange(len(scores_list[0]))
+    for scores in scores_list:
+        plt.plot(n, scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode')
+    plt.show()
+    plt.savefig('dqn2.png')
 
 
 if __name__ == '__main__':
